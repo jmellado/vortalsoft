ed0ba9cc232799b30987c30ce2d0940e0f77c022
jmellado <je_in_er@hotmail.com>
2015-06-17 09:45:20 -0500

    version 1.7.4 jm
    materias update

--- application/views/materias/update.php
+++ application/views/materias/update.php
@@ -117,13 +117,21 @@
 																								{
 																								?>
                                                                                                     <option value="1" selected >1</option>
-	                                                                                                <option value="2">2</option>	
+	                                                                                                <option value="2">2</option>
+	                                                                                                <option value="3">3</option>
+	                                                                                                <option value="4">4</option>
+	                                                                                                <option value="5">5</option>
+	                                                                                                <option value="6">6</option>	
 	                                                                                         
                                                                                                 <?php 
 																								} else {
 																								 ?>
 																								    <option value="1">1</option>
-                                                                                                    <option value="2"  selected >2</option>	
+                                                                                                    <option value="2"  selected >2</option>
+                                                                                                    <option value="3">3</option>
+	                                                                                                <option value="4">4</option>
+	                                                                                                <option value="5">5</option>
+	                                                                                                <option value="6">6</option>
                                                                                          	  
 																							   <?php
 																							    } 
